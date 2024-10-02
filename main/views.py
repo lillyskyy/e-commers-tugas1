84e50bf4d627d792e545aec86e69c62af97ba2aa
lillyskyy <nandociyek@gmail.com>
2024-10-02 14:13:00 +0700

    cobaaa

--- main/views.py
+++ main/views.py
@@ -12,6 +12,8 @@ from django.contrib.auth.decorators import login_required
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 class AdditionalEntryForm(ModelForm):
     class Meta:
@@ -82,7 +84,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-
+      else:
+            messages.error(request, "Invalid username or password. Please try again.")
    else:
       form = AuthenticationForm(request)
    context = {'form': form}
